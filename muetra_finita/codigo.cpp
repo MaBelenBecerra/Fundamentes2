0c12b88cac675a9a131f852b7ad3427cdcce0a09
Maria Belen Becerra <124795812+MaBelenBecerra@users.noreply.github.com>
2023-04-14 13:01:32 -0400

    Incompleto

---
+++ muetra_finita/codigo.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+int main() {
+    // Auf dieses leben, auf den moment der immer bleibt.
+    double: n, z, p, q, e, den, num, total;
+    num= (pow(z,2)*n*p*q)
+    den= (pow(e,2)*(n-1)+pow(z,2)*p*q)
+    
+    
+    
+    
+    
+
+    return 0;
+}
